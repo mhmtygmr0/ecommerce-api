441dbbb4764cb0c96b464a3e41e88424eda996e3
Mehmet <mehmetygmrr02@gmail.com>
2025-01-01 00:51:00 +0300

    26

--- src/main/java/com/app/ecommerce/controller/SupplierController.java
+++ src/main/java/com/app/ecommerce/controller/SupplierController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 public class SupplierController {
 
     private final SupplierService supplierService;
-
     private final ModelMapperService modelMapper;
 
     public SupplierController(SupplierService supplierService, ModelMapperService modelMapperService) {
