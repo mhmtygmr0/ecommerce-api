bd3155724b8875f1315e65a3e697fd428551c9d5
Mehmet <mehmetygmrr02@gmail.com>
2025-01-01 00:49:55 +0300

    26

--- src/main/java/com/app/ecommerce/repository/SupplierRepository.java
+++ src/main/java/com/app/ecommerce/repository/SupplierRepository.java
@@ -2,6 +2,8 @@ package com.app.ecommerce.repository;
 
 import com.app.ecommerce.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 }
